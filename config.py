7db60744cd8eb93cfe704410eb4666b3fe6846fc
Lonndy007 <133030964+Lonndy007@users.noreply.github.com>
2023-06-24 23:04:48 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = "6038108983:AAF2rB9JKtzaBVGN5OrQzMbb1Py45LZ2FS8"
+
+keys = {"рубль":"RUB","эфириум":"ETH","биткоин":"BTC","доллар":"USD","тенге":"KZT","лиры":"TRY","евро":"EUR","белрубль":"BYN"}
